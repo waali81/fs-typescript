cb00f7337523ba72992fd5d6889a4232816a5522
Timo Waali <waali.timo@gmail.com>
2026-08-20 12:01:41 +0300

    9.5

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
     const bmi = weight / (height / 100) ** 2;
 
     if (bmi < 18.5) {
@@ -16,17 +16,19 @@ const calculateBmi = (height: number, weight: number): string => {
     return 'Obese';
 };
 
-try {
-  const height = Number(process.argv[2]);
-  const weight = Number(process.argv[3]);
+if (process.argv[1] === import.meta.filename) {
+  try {
+    const height = Number(process.argv[2]);
+    const weight = Number(process.argv[3]);
 
-  if (isNaN(height) || isNaN(weight)) {
-    throw new Error('Provided values were not numbers!');
-  }
+    if (isNaN(height) || isNaN(weight)) {
+      throw new Error('Provided values were not numbers!');
+    }
 
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  if (error instanceof Error) {
-    console.log('Something went wrong:', error.message);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log('Something went wrong:', error.message);
+    }
   }
 }
--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,9 +1,39 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator.ts';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  if (typeof req.query.height !== 'string' || typeof req.query.weight !== 'string') {
+    return res.status(400).json({
+      error: 'malformatted parameters'
+    });
+  }
+
+  if (req.query.height === '' || req.query.weight === '') {
+    return res.status(400).json({
+      error: 'malformatted parameters'
+    });
+  }
+
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (isNaN(height) || isNaN(weight)) {
+    return res.status(400).json({
+      error: 'malformatted parameters'
+    });
+  }
+
+  return res.json({
+    weight,
+    height,
+    bmi: calculateBmi(height, weight)
+  });
 });
 
 const PORT = 3003;

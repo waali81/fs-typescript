3b7083e5dc3ea185bd724297141ba06336a4fbbd
Timo Waali <waali.timo@gmail.com>
2026-08-25 10:53:21 +0300

    9.13

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
+import { v1 as uuid } from 'uuid';
 import patients from '../../data/patients.ts';
-import type { NonSensitivePatient } from '../types.ts';
+import type { NonSensitivePatient, NewPatient, Patient } from '../types.ts';
 
 const getPatients = (): NonSensitivePatient[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -11,6 +12,17 @@ const getPatients = (): NonSensitivePatient[] => {
   }));
 };
 
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient: Patient = {
+    id: uuid(),
+    ...patient
+  };
+
+  patients.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getPatients,
+  addPatient,
 };
